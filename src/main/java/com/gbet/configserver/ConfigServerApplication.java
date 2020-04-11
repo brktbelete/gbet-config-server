2b4f904810a98f8dd5a4c771d9ab2a4ff9ddab6e
Bereket Belete <bbelete@apposit.com>
2020-04-11 19:47:06 +0300

    disable server side  decryption

--- src/main/java/com/gbet/configserver/ConfigServerApplication.java
+++ src/main/java/com/gbet/configserver/ConfigServerApplication.java
@@ -3,9 +3,11 @@ package com.gbet.configserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 @SpringBootApplication
 @EnableConfigServer
+@RefreshScope
 public class ConfigServerApplication {
 
 	public static void main(String[] args) {
